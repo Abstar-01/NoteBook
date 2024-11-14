637797fd6ef8cecb2732b1dad679c1eb23565d24
Abstar-01 <abgirma03@gmail.com>
2024-11-14 17:13:46 +0300

    Database Initalization

--- NoteBook/ApplicationLogInFrameWork/Form1.cs
+++ NoteBook/ApplicationLogInFrameWork/Form1.cs
@@ -1,25 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿global using System;
+global using System.Drawing;
+global using System.Drawing.Drawing2D;
+global using System.Windows.Forms;
+global using Rounded;
 
 namespace NoteBook{
     public partial class Form1 : Form {
         private Button b;
         private Timer t;
+        private Image backgroundImage;
+        private Label backgroundImageLable;
         public Form1() {
             InitializeComponent();
             this.FormBorderStyle = FormBorderStyle.None;
-            this.Size = new Size(500, 400);
+            this.Size = new Size(1199, 670);
             this.StartPosition = FormStartPosition.CenterScreen;
             this.Load += new EventHandler(this.CustomForm_Load);
 
+            backgroundImage = Image.FromFile("C:\\Users\\user\\RiderProjects\\NoteBook\\NoteBook\\ApplicationLogInFrameWork\\Images & Icons\\Images\\LoginPage.jpg");
+            backgroundImageLable = new Label();
+            backgroundImageLable.Image = backgroundImage;
+            backgroundImageLable.SetBounds(0,-3,1199,676);
+            
+            LoginPanel pane = new LoginPanel(50,Color.FromArgb(30,0,0,0),Color.FromArgb(30,0,0,0));
+            pane.SetBounds(-1,-1,1201,672);
+            pane.BackColor = Color.FromArgb(0, 0, 0, 0);
+
+
+            backgroundImageLable.Controls.Add(pane);
+            this.Controls.Add(backgroundImageLable);
+            
         }
 
         private void CustomForm_Load(object sender, EventArgs e) {
@@ -41,29 +51,35 @@ namespace NoteBook{
     }
 }
 
-class RoundPanel : Panel {
-    int radius;
-    private Color start, end;
-    public RoundPanel(int radius, Color start, Color end) {
-        this.radius = radius;
-        this.start = start;
-        this.end = end;
-    }
-    protected override void OnPaint(PaintEventArgs e) {
-        base.OnPaint(e);
-        GraphicsPath path = new GraphicsPath();
-        path.StartFigure();
-        path.AddArc(new Rectangle(0, 0, radius, radius), 180, 90);
-        path.AddArc(new Rectangle(this.Width - radius, 0, radius, radius), 270, 90);
-        path.AddArc(new Rectangle(this.Width - radius, this.Height - radius, radius, radius), 0, 90);
-        path.AddArc(new Rectangle(0, this.Height - radius, radius, radius), 90, 90);
-        path.CloseFigure();
-            
-        this.Region = new Region(path);
+namespace Rounded
+{
+    public class RoundPanel : Panel {
+        int radius;
+        private Color start, end;
 
-        using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle,this.start, this.end, LinearGradientMode.ForwardDiagonal)) {
-            e.Graphics.FillRectangle(brush, this.ClientRectangle);
+        public RoundPanel(int radius, Color start, Color end) {
+            this.radius = radius;
+            this.start = start;
+            this.end = end;
         }
-        
+
+        protected override void OnPaint(PaintEventArgs e){
+            base.OnPaint(e);
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(new Rectangle(0, 0, radius, radius), 180, 90);
+            path.AddArc(new Rectangle(this.Width - radius, 0, radius, radius), 270, 90);
+            path.AddArc(new Rectangle(this.Width - radius, this.Height - radius, radius, radius), 0, 90);
+            path.AddArc(new Rectangle(0, this.Height - radius, radius, radius), 90, 90);
+            path.CloseFigure();
+
+            this.Region = new Region(path);
+
+            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, this.start, this.end,
+                       LinearGradientMode.ForwardDiagonal)) {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+
+        }
     }
 }
--- NoteBook/ApplicationLogInFrameWork/LoginPanel.cs
+++ NoteBook/ApplicationLogInFrameWork/LoginPanel.cs
@@ -1,7 +1,16 @@
-﻿namespace NoteBook
+﻿using System.Windows.Forms;
+using NoteBook;
+namespace NoteBook
 {
-    public class LoginPanel
-    {
-        
+    public class LoginPanel : RoundPanel {
+        private Label Title;
+        private RoundPanel signin;
+        public LoginPanel(int x, Color s, Color e) : base(x,s,e) {
+            Title = new Label();
+            Title.Text = "People move around";
+            Title.SetBounds(50,50,100,50);
+            Title.BackColor = Color.Transparent;
+            this.Controls.Add(Title);
+        }
     }
 }
