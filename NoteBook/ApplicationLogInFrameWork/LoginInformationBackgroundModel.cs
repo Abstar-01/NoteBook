f84bfb23b52b463cf374751759e54d3d8a99cecd
Abstar-01 <abgirma03@gmail.com>
2024-11-17 11:48:22 +0300

    Working on the Authentication Structure of the System

--- NoteBook/ApplicationLogInFrameWork/LoginInformationBackgroundModel.cs
+++ NoteBook/ApplicationLogInFrameWork/LoginInformationBackgroundModel.cs
@@ -9,9 +9,9 @@ public class LoginInformationBackgroundModel {
         
     }
 
-    public static void Main(string[] args)
-    {
+    public static void Main(string[] args) {
         LoginInformationBackgroundModel lib = new LoginInformationBackgroundModel();
+        lib.NewAccount();
     }
 
     public void SearchAccount() {
@@ -30,7 +30,42 @@ public class LoginInformationBackgroundModel {
     public void NewAccount(){
         using (SqlConnection con = new SqlConnection(connection)) {
             try {
+                con.Open();
+                string user;
+                bool check = false;
                 
+                string usr = "TnTen";
+                string phone = "2";
+                
+                    string s = "SELECT UserName, PhoneNumber FROM UserInformation";
+                    SqlCommand com = new SqlCommand(s,con);
+                    
+                    using (SqlDataReader reader = com.ExecuteReader()) {
+                        while (reader.Read()) {
+                            if (usr == reader.GetString(0) || phone == reader.GetString(1)) {
+                                check = true;
+                                break;
+                            }
+                        }
+                    }
+
+                if (!check) {
+                    string inputing =
+                        "INSERT INTO UserInformation(UserName, FirstName, LastName, PhoneNumber, Email)" +
+                        "VALUES (@usr,@FN,@LN,@PN,@EM)";
+                    using (SqlCommand cmd = new SqlCommand(inputing,con)) {
+                        cmd.Parameters.AddWithValue("@usr", usr);
+                        cmd.Parameters.AddWithValue("@FN", "Abel");
+                        cmd.Parameters.AddWithValue("@LN", "Girma");
+                        cmd.Parameters.AddWithValue("@PN",phone);
+                        cmd.Parameters.AddWithValue("@EM", usr + "@gmail.com");
+
+                        int rows = cmd.ExecuteNonQuery();
+                        Console.WriteLine("Done");
+                    }
+                }else {
+                    Console.WriteLine("Username or phonenumber has already been entered");   
+                }
             }catch (Exception e) {
                 Console.WriteLine("Error opening the database");
             }
--- NoteBook/DataBase/Authentication.sql
+++ NoteBook/DataBase/Authentication.sql
@@ -2,13 +2,20 @@ CREATE DATABASE NoteBook_Authentication
 USE NoteBook_Authentication
 
 CREATE TABLE AuthenticationInformation(
-	UserName VARCHAR(50) NOT NULL,
-	PasswordHash VARCHAR(100) NOT NULL
-	PRIMARY KEY(UserName)
+	UserName VARCHAR(50)NOT NULL,
+	Email VARCHAR(50),
+	PasswordHash VARCHAR(100) NOT NULL,
+	PRIMARY KEY(UserName),
+	FOREIGN KEY (UserName) REFERENCES UserInformation(UserName),
+	FOREIGN KEY (Email) REFERENCES UserInformation(Email)
 )
 
 CREATE TABLE UserInformation(
+	UserName VARCHAR(50) NOT NULL,
 	FirstName VARCHAR(50),
 	LastName VARCHAR(50),
-	UserName VARCHAR(50),
+	PhoneNumber VARCHAR(50) UNIQUE,
+	Email VARCHAR(50) UNIQUE,
+	PRIMARY KEY(UserName)
 )
+
